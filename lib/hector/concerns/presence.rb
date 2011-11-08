a1a8d280831fe97ff48bfbff11c8355757ca67b7
bpaf <edi@mbp.(none)>
2011-11-08 15:57:25 +0100

    send MOTD from config/motd.txt

--- lib/hector/concerns/presence.rb
+++ lib/hector/concerns/presence.rb
@@ -36,8 +36,17 @@ module Hector
 
       protected
         def deliver_welcome_message
-          respond_with("001", nickname, :text => "Welcome to IRC")
-          respond_with("422", :text => "MOTD File is missing")
+          respond_with("001", nickname, :text => "Welcome to #{Hector.server_name}")
+          welcome_message_path = Hector.root.join("config/motd.txt")
+          if File.exist?(welcome_message_path)
+            respond_with("375", :text => "#{Hector.server_name} IRC Message Of The Day")
+            File.foreach(welcome_message_path) { |line|
+              respond_with("372", :text => line)
+            }
+            respond_with("376", :text => "End of MOTD")
+          else
+            respond_with("422", :text => "MOTD File missing")
+          end
         end
 
         def deliver_quit_message
